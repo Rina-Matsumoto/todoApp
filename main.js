7e996dcd099586a3bc31b6dcfab1ea49f77eb7a6
Rina-Matsumoto <ri0322@stu.kanazawa-u.ac.jp>
2023-10-24 16:05:51 +0900

    ローカルストレージにinput値を保存する

--- main.js
+++ main.js
@@ -2,13 +2,31 @@
 
 {
   
+  // 追加ボタンを押したときに入力値を受け取って表示させる
+  // ☓ボタンを押したときに削除する
+  // ページを再読込したときに値を保持する（inputの値）
+  // ページを再読込したときに値を保持する（checkboxの値）
+  // チェックされたものをリセットボタンでまとめて削除する
 
 
-  // renderTodoは、todoを引数にとり、HTMLの各要素を作成して値を入れる
+
+  let todos;
+
+
+  // ローカルストレージのtodosというキーに対応する値がnullの場合、変数todosに空の配列を入れる。nullでなければ、ローカルストレージのtodosの値を取得し、オブジェクトに変換して配列に入れる。
+  if (localStorage.getItem('todos') === null) {
+    todos = [];
+  } else {
+    todos = JSON.parse(localStorage.getItem('todos'));
+  }
+
+
+  // renderTodoは、todoを引数にとり、HTMLの各要素を作成して値を入れる（タグと中身を作る）
   const renderTodo = (todo)=>{
     const input = document.createElement('input');
     input.type = 'checkbox';
     input.checked = todo.isCompleted;
+    
 
     const span = document.createElement('span');
     span.textContent = todo.title;
@@ -29,13 +47,20 @@
     const li = document.createElement('li');
     li.appendChild(lavel);
     li.appendChild(button);
-
-
     document.querySelector('#todos').appendChild(li);
-
   }
 
-  // 追加ボタンが押されたときの処理、todoを引数としてrenderTodoを呼び出す。
+
+  // 配列todosを展開し、HTMLを作っている
+  const renderTodos = () => {
+    todos.forEach((todo) => {
+      renderTodo(todo);
+    });
+  };
+
+
+
+  // 追加ボタンが押されたときの処理、todo(入力値を含む)を引数としてrenderTodoを呼び出す。
   document.querySelector('#add-form').addEventListener('submit', (e)=>{
     e.preventDefault();
     const input = document.querySelector('#add-form input');
@@ -43,11 +68,22 @@
       title: input.value,
       isCompleted: false,
     }
+    // todoオブジェクトのデータを使って、HTML要素を作る
     renderTodo(todo);
+
+    // ここからローカルストレージのための処理
+    // 配列todosに追加する
+    todos.push(todo);
+    // ローカルストレージの更新
+    localStorage.setItem('todos', JSON.stringify(todos));
+    
     input.value = '';
     input.focus();
   });
 
+
+  // これによって、配列todosが展開され、それぞれのオブジェクトにrenderTodoがはしる。
+  renderTodos();
   
 
   
